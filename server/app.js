36b0c05df42c09e9a646a990d83eef148dc9a7fd
Hendrikus van Katwijk <hendrikus@resttech.io>
2020-05-15 15:46:10 +0200

    init client vue

--- server/app.js
+++ server/app.js
@@ -1,2 +1,16 @@
 const Express = require("express");
-const Http = require("")
+const Http = require("http").Server(Express);
+const Socketio = require("socket.io")(Http);
+
+var position = {
+    x: 200,
+    y: 200
+};
+
+Socketio.on("connection", socket =>{
+    socket.emit("position",position)
+});
+
+Http.listen(3000, () => {
+    console.log("Listening at :3000....");
+})
