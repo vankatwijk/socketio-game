ffef268c86348a90797e867e4377c56808b0b0ca
Hendrikus van Katwijk <hendrikus@resttech.io>
2020-05-15 16:36:46 +0200

    added canvas +basic vue code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BlockGame />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/BlockGame.vue'
+import BlockGame from './components/BlockGame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BlockGame
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- client/src/components/BlockGame.vue
+++ client/src/components/BlockGame.vue
@@ -1,58 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+
+    <canvas 
+    ref="game" 
+    width="640" 
+    height="480" 
+    style="border:1px solid red;">
+    </canvas>
   </div>
 </template>
 
 <script>
+import io from "socket.io-client"
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'BlockGame',
+  data(){
+    return{
+      socket:{},
+      context:{},
+      position:{
+        x:0,
+        y:0
+      }
+    }
+  },
+  created(){
+    this.socket = io("http://localhost:3000");
+  },
+  mounted(){
+    this.context = this.$refs.game.getContext("2d");
+    this.socket.on("position",data =>{
+      this.position = data;
+      this.context.clearRect(0,0,this.$refs.game.width,this.$refs.game.height);
+      this.context.fillRect(this.position.x, this.position.y, 20, 20);
+    })
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
